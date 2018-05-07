8e110cb18f79af39366f523c7caae26bad967d13
NicholasPhillips <nick@red-river-software.co.uk>
2018-05-07 12:25:23 +0100

    Attempting to clone from a list

--- src/generator.rs
+++ src/generator.rs
@@ -5,9 +5,20 @@ use models;
 pub fn seed_list(size:i32) -> Vec<models::Item> {
     let mut rng = rand::thread_rng();
     (0..size).map(|_| {
-        models::Item {
-            weight: rng.gen_range(0, 100),
-            value: rng.gen_range(0, 100)
-        }
+        let items = items();
+        items[rng.gen_range(0, 9)]
     }).collect::<Vec<_>>()
+}
+
+pub fn items() -> Vec<models::Item> {
+    vec![
+     models::Item {weight:12,value:8},
+        models::Item {weight:7,value:7},
+        models::Item {weight:4,value:1},
+        models::Item {weight:9,value:5},
+        models::Item {weight:10,value:12},
+        models::Item {weight:3,value:3},
+        models::Item {weight:2,value:3},
+        models::Item {weight:2,value:2},
+        models::Item {weight:5,value:10}]
 }
